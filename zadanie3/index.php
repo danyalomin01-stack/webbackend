df8332011ebdfdcecf5ccb0f01c7889c73c8ea17
Данил Правдин <pravdin@MacBook-Air-Danil.local>
2026-05-12 08:37:58 +0300

    1

--- zadanie3/index.php
+++ zadanie3/index.php
@@ -3,7 +3,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 // ─── Настройки БД ──────────────────────────────────────────────
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u82410');   // ваш логин, он же имя БД
+define('DB_NAME', 'uu82410');   // ваш логин, он же имя БД
 define('DB_USER', 'u82410');   // ваш логин
 define('DB_PASS', '4348747'); // ваш пароль
 
